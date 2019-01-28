608c76be43d98c5540088fd2bd0cf25773a5a0fc
r00telement <r00telement@users.noreply.github.com>
2019-01-28 23:31:30 +0000

    Adjusted default positions of widgets because they are off screen for some people

--- SmartHunter/Game/Config/OverlayConfig.cs
+++ SmartHunter/Game/Config/OverlayConfig.cs
@@ -9,8 +9,8 @@ namespace SmartHunter.Game.Config
         public float ScaleStep = 0.1f;
         public bool HideWhenGameWindowIsInactive = false;
 
-        public TeamWidgetConfig TeamWidget = new TeamWidgetConfig(1540, 715);
-        public MonsterWidgetConfig MonsterWidget = new MonsterWidgetConfig(517, 143);
-        public WidgetConfig PlayerWidget = new WidgetConfig(435, 803);
+        public TeamWidgetConfig TeamWidget = new TeamWidgetConfig(220, 220);
+        public MonsterWidgetConfig MonsterWidget = new MonsterWidgetConfig(320, 120);
+        public WidgetConfig PlayerWidget = new WidgetConfig(120, 320);
     }
 }
